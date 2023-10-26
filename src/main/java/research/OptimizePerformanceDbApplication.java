af4e33f764ea7d83e51b7fce2f2dcc616834d17b
Phong <phongpham.dev@gmail.com>
2023-10-26 19:59:51 +0700

    init commit

---
+++ src/main/java/research/OptimizePerformanceDbApplication.java
@@ -0,0 +1,13 @@
+package research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizePerformanceDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizePerformanceDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/research/OptimizePerformanceDbApplicationTests.java
@@ -0,0 +1,13 @@
+package research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizePerformanceDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
